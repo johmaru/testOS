7d136cb509d1e801b712a17b8cda32b66e376221
Jo Matsumaru <hatunehimeka@outlook.jp>
2025-05-19 22:41:28 +0900

    allocatorモジュールを追加し、メモリ管理機能を実装。Cargo.tomlにプロファイル設定を追加。lib.rsにeh_personality関数を追加し、テスト環境を整備。uefi.rsに物理メモリ開始アドレスを取得する関数を追加。

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![feature(offset_of)]
 #![feature(custom_test_frameworks)]
+#![feature(lang_items)]
 #![test_runner(test_runner::test_runner)]
 #![reexport_test_harness_main = "run_unit_tests"]
 #![no_main]
 
+pub mod allocator;
 pub mod graphics;
 pub mod qemu;
 pub mod result;
@@ -13,6 +15,11 @@ pub mod x86;
 
 #[cfg(test)]
 pub mod test_runner;
+
+#[cfg(test)]
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
 
 #[cfg(test)]
 #[no_mangle]
